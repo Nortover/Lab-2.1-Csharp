02630dd2e4e421179444f30d3fa7d99b44e3284a
Стас Пушко <staspushko7@gmail.com>
2025-05-12 19:15:29 +0300

    Add project files.

---
+++ Lab-2.1-Csharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using StringLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Демонстрація роботи з базовим класом
+        Console.WriteLine("Введiть рядок:");
+
+        StringBase baseString = new StringBase(Console.ReadLine());
+
+        Console.WriteLine($"Рядок: {baseString.Value}");
+
+        Console.WriteLine($"Довжина рядка: {baseString.CalculateLength()}");
+
+        // Демонстрація роботи з похідним класом
+        Console.WriteLine("Введiть рядок з символом '-'");
+        NumericString numString = new NumericString(Console.ReadLine());
+        Console.WriteLine($"Початковий рядок: {numString.GetStringValue()}");
+        Console.WriteLine($"Довжина рядка: {numString.CalculateLength()}");
+
+        // Видаляємо дефіси
+        numString.RemoveCharacter('-');
+        Console.WriteLine($"Рядок пiсля видалення '-': {numString.GetStringValue()}");
+        Console.WriteLine($"Довжина рядка пiсля змiн: {numString.CalculateLength()}");
+
+    }
+}
---
+++ Lab-2.1-Csharp/String.cs
@@ -0,0 +1,74 @@
+﻿
+namespace StringLibrary
+{
+    public class StringBase
+    {
+        protected string value;
+
+        // Конструктор за замовчуванням
+        public StringBase()
+        {
+            value = string.Empty;
+        }
+
+        // Конструктор з параметром
+        public StringBase(string str)
+        {
+            value = str ?? string.Empty;
+        }
+
+        // Копіюючий конструктор
+        public StringBase(StringBase other)
+        {
+            value = other.value ?? string.Empty;
+        }
+
+        // Властивість для читання значення рядка
+        public string Value
+        {
+            get { return value; }
+        }
+
+        // Метод обчислення довжини рядка
+        public virtual int CalculateLength()
+        {
+            return value.Length;
+        }
+    }
+    public class NumericString : StringBase
+    {
+        // Конструктор за замовчуванням 
+        public NumericString() : base()
+        {
+        }
+
+        // Конструктор з параметром 
+        public NumericString(string str) : base(str)
+        {
+        }
+
+        // Копіюючий конструктор 
+        public NumericString(NumericString other) : base(other)
+        {
+        }
+
+
+        // Метод для отримання довжини рядка 
+        public override int CalculateLength()
+        {
+            return base.CalculateLength();
+        }
+
+        // Додаткові методи для отримання даних об'єкта
+        public string GetStringValue()
+        {
+            return Value;
+        }
+
+        // Метод видалення заданого символу з рядка
+        public void RemoveCharacter(char charToRemove)
+        {
+            value = value.Replace(charToRemove.ToString(), "");
+        }
+    }
+}
